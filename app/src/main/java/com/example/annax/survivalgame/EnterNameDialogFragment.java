d8f86b86324868feda40e858267f2af549efa412
annywang0106 <annywang0106@gmail.com>
2017-10-24 02:15:19 -0400

    Added scores function

---
+++ app/src/main/java/com/example/annax/survivalgame/EnterNameDialogFragment.java
@@ -0,0 +1,54 @@
+package com.example.annax.survivalgame;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.text.InputType;
+import android.widget.EditText;
+
+import java.util.Random;
+
+
+public class EnterNameDialogFragment extends DialogFragment {
+    private String mEditText;
+    private SharedPreferences sharedPref = null;
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Enter your name:");
+
+// Set up the input
+        final EditText input = new EditText(getActivity());
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mEditText = input.getText().toString();
+                Random r = new Random();
+                int score = r.nextInt(100 - 50) + 50;
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt(mEditText, score);
+                editor.commit();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        return builder.create();
+    }
+
+}
--- app/src/main/java/com/example/annax/survivalgame/ScoreFragment.java
+++ app/src/main/java/com/example/annax/survivalgame/ScoreFragment.java
@@ -1,17 +1,24 @@
 package com.example.annax.survivalgame;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.example.annax.survivalgame.SingleFragmentActivity.enableSoundEffect;
 
@@ -22,34 +29,60 @@ import static com.example.annax.survivalgame.SingleFragmentActivity.enableSoundE
 public class ScoreFragment extends Fragment implements View.OnClickListener{
 
     private MediaPlayer mp = null;
+    private SharedPreferences sharedPref = null;
+    LinearLayout linearLayout = null;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.score_fragment, container, false);
-
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
         mp = MediaPlayer.create(getActivity(), R.raw.sound_effect1);
+        linearLayout = v.findViewById(R.id.scoreHistory);
+        setScoreHistory();
+        Button enterName = v.findViewById(R.id.enterName);
         Button scoreBack = v.findViewById(R.id.ScoreBack);
         Button facebook = v.findViewById(R.id.Facebook);
+        enterName.setOnClickListener(this);
         scoreBack.setOnClickListener(this);
         facebook.setOnClickListener(this);
         return v;
     }
 
-    public void onClick(View view){
-        if(view.getId()==R.id.ScoreBack){
-            if(enableSoundEffect){
+    public void onClick(View view) {
+        if (view.getId() == R.id.ScoreBack) {
+            if (enableSoundEffect) {
                 mp.start();
             }
             getActivity().finish();
-        } else if (view.getId() == R.id.Facebook){
-            if(enableSoundEffect){
+        } else if (view.getId() == R.id.Facebook) {
+            if (enableSoundEffect) {
                 mp.start();
             }
             Uri facebook = Uri.parse("https://www.facebook.com/");
             Intent fIntent = new Intent(Intent.ACTION_VIEW, facebook);
             PackageManager packageManager = getActivity().getPackageManager();
             List<ResolveInfo> activities = packageManager.queryIntentActivities(fIntent, 0);
-            if( activities.size() > 0) {
+            if (activities.size() > 0) {
                 startActivity(fIntent);
             }
+        } else if (view.getId() == R.id.enterName) {
+            showEnterNameDialog();
+        }
+    }
+    private void showEnterNameDialog() {
+        FragmentManager manager = getFragmentManager();
+        EnterNameDialogFragment fragment = new EnterNameDialogFragment();
+        fragment.show(manager, "enter_name");
+    }
+
+    private void setScoreHistory(){
+        Map<String,?> keys = sharedPref.getAll();
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            TextView textView = new TextView(getActivity());
+            textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            textView.setTextSize(25);
+            textView.setText(entry.getKey() + " : " +
+                    entry.getValue().toString());
+            linearLayout.addView(textView);
         }
     }
 
