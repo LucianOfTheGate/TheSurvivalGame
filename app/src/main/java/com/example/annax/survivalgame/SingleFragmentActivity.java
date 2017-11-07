90b030ff699b600254bb05a5a0a852d18a7cfb23
annywang0106 <annywang0106@gmail.com>
2017-11-06 23:28:44 -0500

    Added Voice Control function into all activities

--- app/src/main/java/com/example/annax/survivalgame/SingleFragmentActivity.java
+++ app/src/main/java/com/example/annax/survivalgame/SingleFragmentActivity.java
@@ -1,10 +1,18 @@
 package com.example.annax.survivalgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
@@ -12,6 +20,8 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     public static boolean enableCommunication_m = true;
     protected abstract Fragment createFragment();
 
+    public ListView mList;
+    public static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
     @LayoutRes
     protected int getLayoutResId() {
         return R.layout.activity_fragment;
@@ -30,6 +40,78 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
+        }
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.voiceControl:
+                startVoiceRecognitionActivity();
+                return true;
+            case R.id.back:
+                this.finish();
+                return true;
+            case R.id.setting:
+                startActivity(new Intent(this.getApplicationContext(), SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speech recognition demo");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Fill the list view with the strings the recognizer thought it
+            // could have heard
+            ArrayList matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+           // mList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, matches));
+            // matches is the result of voice input. It is a list of what the
+            // user possibly said.
+            // Using an if statement for the keyword you want to use allows the
+            // use of any activity if keywords match
+            // it is possible to set up multiple keywords to use the same
+            // activity so more than one word will allow the user
+            // to use the activity (makes it so the user doesn't have to
+            // memorize words from a list)
+            // to use an activity from the voice input information simply use
+            // the following format;
+            // if (matches.contains("keyword here") { startActivity(new
+            // Intent("name.of.manifest.ACTIVITY")
+
+            if (matches.contains("back")) {
+                this.finish();
+            }
+            if (matches.contains("menu")) {
+                this.finish();
+            }
+            if (matches.contains("score")) {
+                this.finish();
+                startActivity(new Intent(this.getApplicationContext(), ScoresActivity.class));
+            }
+            if (matches.contains("setting")) {
+                this.finish();
+                startActivity(new Intent(this.getApplicationContext(), SettingsActivity.class));
+            }
+
         }
     }
 }
